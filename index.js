9189665bf7ac0fc4d4d5aadc6ea82484f779ea14
addempsea <babatundeademola4@gmail.com>
2022-07-09 01:25:14 +0100

    feat: 🎸 add auth to sub and pub

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import aedes from "aedes";
 import { createServer } from "net";
+import pgPromise from "pg-promise";
+import promise from "bluebird";
+import dotenv from 'dotenv';
+
+dotenv.config();
 const port = 1889;
 
 const aedesServer = aedes();
@@ -7,61 +12,67 @@ createServer(aedesServer.handle).listen(port, () => {
   console.log(`MQTT Broker running on port: ${port}`);
 });
 
-const users = [
-  {
-    id: null,
-    username: "demolala",
-    password: "demo123",
-    topics: [],
-  },
-];
+const pgp = pgPromise({ promiseLib: promise, noLocking: true });
+
+const envs = {
+  dbUrl: process.env.DB_URL,
+}
+
+const db = pgp(envs.dbUrl);
 
 /**
  * FindClientById is a function that takes a client object as an argument and returns the user object
  * that matches the client's id
  * @param client - The client object that we want to find in the users array.
  */
-const findClientById = (client) => users.find((el) => el.id === client.id);
+ const findById = async (id) =>  db.oneOrNone(`SELECT * FROM mqtt_user_info WHERE id = $1`, [id]);
 
 /* A callback function that is called when a client tries to connect to the broker. */
 aedesServer.authenticate = (client, username, password, callback) => {
-  console.log(client);
-  password = Buffer.from(password, "base64").toString();
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
-  if (user) {
-    const index = users.findIndex((el) => el.username === username);
-    users[index].id = client.id;
-    return callback(null, true);
-  }
-  const error = new Error("Authentication Failed!! Invalid user credentials.");
-  console.log("Error ! Authentication failed.");
-  return callback(error, false);
+  console.log({ client, id: client.id, username, password });
+  const decryptedPassword = Buffer.from(password, "base64").toString();
+  findById(client.id).then((user) => {
+    console.log({ user });
+    if (user.username === username && user.password === decryptedPassword) {
+      return callback(null, true);
+    }
+    const error = new Error("Authentication Failed!! Invalid user credentials.");
+    console.log("Error ! Authentication failed.");
+    return callback(error, false);
+  });
+  
 };
 
 /* This is the function that is called when a client publishes a message packet on the topic. */
 aedesServer.authorizePublish = (client, packet, callback) => {
-  const clientData = findClientById(client);
-  if (clientData) {
-    return callback(null);
-  }
-  console.log("Error ! Unauthorized publish to a topic.");
-  return callback(
-    new Error("You are not authorized to publish on this message topic.")
-  );
+  findById(client.id).then((user) => {
+    if (user.topics) {
+      const parsedTopics = user.topics.split(",");
+      if (parsedTopics.includes(packet.topic)) {
+        return callback(null, packet);
+      }
+      console.log("Error ! Unauthorized publish to a topic.");
+      return callback(
+        new Error("You are not authorized to publish to this message topic.")
+      );
+    }
+  })
 };
 
 /* This is the function that is called when a client subscribes to a topic. */
 aedesServer.authorizeSubscribe = (client, sub, callback) => {
-  const clientData = findClientById(client);
-  if (clientData.topics.includes(sub.topic)) {
-    return callback(null, sub);
-  }
-  console.log("Error ! Unauthorized subscribe to a topic.");
-  return callback(
-    new Error("You are not authorized to subscribe to this message topic.")
-  );
+  findById(client.id).then((user) => {
+    if (user.topics) {
+      const parsedTopics = user.topics.split(",");
+      if (parsedTopics.includes(sub.topic)) {
+        return callback(null, sub);
+      }
+      console.log("Error ! Unauthorized subscribe to a topic.");
+      return callback(
+        new Error("You are not authorized to subscribe to this message topic.")
+      );
+    }
+  })
 };
 
 // emitted when a client connects to the broker
@@ -112,7 +123,5 @@ aedesServer.on("publish", async function (packet, client) {
         client ? client.id : "BROKER_" + aedesServer.id
       } has published message on ${packet.topic} to broker ${aedesServer.id}`
     );
-    const clientData = findClientById(client);
-    clientData.topics.push(packet.topic);
   }
 });
